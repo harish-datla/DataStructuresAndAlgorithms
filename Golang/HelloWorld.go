40625a9e084cc88bacc10034894ce20522bcecd0
harish-datla <36021808+harish-datla@users.noreply.github.com>
2024-02-06 01:35:43 +0530

    feat: hello world example

---
+++ Golang/HelloWorld.go
@@ -0,0 +1,18 @@
+package main
+
+// Every Go program starts with a package declaration.
+// Package main is required for a standalone executable.
+
+import "fmt"
+
+// Import package "fmt" to use Println() function
+
+/* This is a Multiple line comment */
+
+// This is a Single line comment
+func main() {
+	// Main function is the starting point of program
+	fmt.Println("Hello, World!")
+	// Function Prinln() is defined in fmt package used to print
+	// "Hello, World!" to the output screen
+}
